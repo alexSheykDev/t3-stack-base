c981b4f99ed5def1419d722be044bd6ed29ff017
oleksii.sheiko <oleksii.sheiko@otakoyi.com>
2025-11-22 16:51:08 +0200

    feat: added admin procedures

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -131,3 +131,15 @@ export const protectedProcedure = t.procedure
       },
     });
   });
+
+export const adminProcedure = protectedProcedure.use(async ({ ctx, next }) => {
+  if (ctx.session.user.role !== "ADMIN") {
+    throw new TRPCError({ code: "FORBIDDEN" });
+  }
+  return next({
+    ctx: {
+      // infers the `session` as non-nullable
+      session: { ...ctx.session, user: ctx.session.user },
+    },
+  });
+});
